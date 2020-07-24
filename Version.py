3e9528c4dda681c1116632532bba52a5829d295c
wadelamble <wadelamble@hotmail.com>
2020-07-23 21:14:29 -0700

    version file

---
+++ Version.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 23 21:01:33 2020
+
+@author: wadel
+"""
+
+import sys
+import numpy as np
+import matplotlib
+
+print(sys.version)
+print(np.__version__)
+print(matplotlib.__version__)
